758fd3008666258d61431eacc118afe1e5f3083d
Sharath R <104022957+sharathr7@users.noreply.github.com>
2022-12-14 20:37:47 +0530

    Add files via upload

---
+++ mealsFinder/script.js
@@ -0,0 +1,28 @@
+const searchInput=document.getElementById('search_input');
+const mealsContainer=document.getElementById('meals_container');
+
+init();
+
+function init(){
+    fetchMeals()
+    searchInput.addEventListener('keyup',(event)=>{
+        if(event.key === 'Enter')
+        {
+            fetchMeals()
+        }
+    })
+}
+
+async function fetchMeals(){
+    const value=searchInput.value;
+    const streamResponse= await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${value}`);
+    const textBody= await streamResponse.text();
+    const jsonData=JSON.parse(textBody);
+
+    let html='';
+    for(let i=0;i<jsonData.meals.length;i++)
+    {
+        html += `<img class="meals_img" src="${jsonData.meals[i].strMealThumb}" alt=""/>`
+    }
+    mealsContainer.innerHTML=html;
+}
